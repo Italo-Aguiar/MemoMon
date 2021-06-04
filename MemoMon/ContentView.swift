49fe472dc264b14bad1464cb457fc571f950e95b
Italo Aguiar <italoaa99@gmail.com>
2021-06-04 02:57:05 -0300

    making it a bit better with grid view and fixing sizes

--- MemoMon/ContentView.swift
+++ MemoMon/ContentView.swift
@@ -6,18 +6,75 @@
 //
 
 import SwiftUI
-
+ 
 struct ContentView: View {
-    var body: some View {
-      ZStack {
-        RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
-          .stroke(lineWidth: 3)
-          
-        Text("Hello, world!")
+  var emojis = ["🚀", "🚴🏻‍♀️", "✈️", "⛵️", "🚲", "🚆", "🚔", "🛵", "🛺", "🚡", "🛴", "🚞", "🚁"]
+  
+  @State var emojiCount = 6
+  
+  var body: some View {
+    VStack {
+      ScrollView {
+        LazyVGrid (columns: [  GridItem(.adaptive(minimum: 65)) ]) {
+          ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+            CardView(isFaceUp: true, content: emoji).aspectRatio(2/3, contentMode: .fit )
+          }
+        }
+      }
+      Spacer()
+      HStack {
+        remove
+        Spacer()
+        add
       }
       .padding()
-      .foregroundColor(.purple)
+      .font(.largeTitle)
     }
+    .padding()
+    .foregroundColor(.purple)
+  }
+  
+  var add: some View {
+    Button(action: {
+      if emojiCount < emojis.count {
+        emojiCount += 1
+      }
+    } , label: {
+      Image(systemName: "plus.circle")
+    })
+  }
+  
+  var remove: some View {
+    Button(action: {
+      if emojiCount > 1 {
+        emojiCount -= 1
+      }
+    } , label: {
+      Image(systemName: "minus.circle")
+    })
+  }
+}
+
+struct CardView: View {
+  @State var isFaceUp: Bool
+  var content: String
+  
+  var body: some View {
+    ZStack {
+      let shape = RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+      
+      if isFaceUp {
+        shape.fill().foregroundColor(.white)
+        shape.strokeBorder(lineWidth: 3)
+        Text(content).font(.largeTitle)
+      } else {
+        shape.fill()
+      }
+    }
+    .onTapGesture {
+      isFaceUp = !isFaceUp
+    }
+  }
 }
 
 
@@ -53,6 +110,10 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+      ContentView()
+        .preferredColorScheme(.dark)
+      
+      ContentView()
+        .preferredColorScheme(.light)
     }
 }
