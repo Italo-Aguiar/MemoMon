3bfd0e1eb29e91931f659e94e5ce50e2ed91aaf4
Italo Aguiar <italoaa99@gmail.com>
2021-06-04 01:10:15 -0300

    Initial Commit

---
+++ MemoMon/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MemoMon
+//
+//  Created by Ítalo Assunção on 04/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MemoMon/MemoMonApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoMonApp.swift
+//  MemoMon
+//
+//  Created by Ítalo Assunção on 04/06/21.
+//
+
+import SwiftUI
+
+@main
+struct MemoMonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
